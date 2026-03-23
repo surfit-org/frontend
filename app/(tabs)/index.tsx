56e35a66a93aaeeaf981d93a903ae6f97113c4d8
jungi Hong <hjg020531@naver.com>
2026-03-23 16:54:13 +0900

    chore: 프론트엔드 초기 세팅

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">surfit</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+});
